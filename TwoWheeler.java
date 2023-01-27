45a2fd94e3bd077b59e566ba368b1d9e40c77abb
SathiyaprakashMK <118544594+SathiyaprakashMK@users.noreply.github.com>
2023-01-27 20:17:10 +0530

    Add files via upload

---
+++ TwoWheeler.java
@@ -0,0 +1,21 @@
+package week1.day2;
+
+public class TwoWheeler {
+
+	public static void main(String[] args) {
+		int noOfWheels=2;
+		short noOfMirrors=2;
+		long chassisNumber=546372819l;
+		boolean isPunctured=false;
+		String bikeName="RE Clasic 350";
+		double runningKM=60000;
+		System.out.println("No. of. Wheels : "+noOfWheels+
+				"\nNo. of. Mirrors : "+noOfMirrors+
+				"\nChassis Number : "+chassisNumber+
+				"\nIs the two wheeler punctured : "+isPunctured+
+				"\nBike Name : "+bikeName+
+				"\nRunning KM : "+runningKM);
+
+	}
+
+}
