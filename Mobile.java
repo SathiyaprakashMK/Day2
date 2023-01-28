2fd4f093719d632b795c0bba25fcc5f211181a26
SathiyaprakashMK <118544594+SathiyaprakashMK@users.noreply.github.com>
2023-01-28 15:58:17 +0530

    Update Mobile.java

--- Mobile.java
+++ Mobile.java
@@ -1,22 +1,22 @@
 package week1.day2;
 
 public class Mobile {
-
-	public static void main(String[] args) {
-		String mobileBrandName="Moto";
+	String mobileBrandName="Moto";
 		char mobileLogo='M';
 		short noOfMobilePiece=10;
 		int modelNumber=8667;
 		long mobileImeiNumber=334455667788l;
 		float mobilePrice=15000f;
 		boolean isDamaged =false;
-		System.out.println("Mobile Brand Name : "+ mobileBrandName);
-		System.out.println("Mobile Logo : "+mobileLogo);
-		System.out.println("No of Mobile Piece : "+noOfMobilePiece);
-		System.out.println("Model Number : "+modelNumber);
-		System.out.println("Mobile IMEI Number : "+mobileImeiNumber);
-		System.out.println("Mobile Price : "+mobilePrice);
-		System.out.println("Is mobile damaged : "+isDamaged);
+	public static void main(String[] args) {
+		Mobile mbl=new Mobile();
+		System.out.println("Mobile Brand Name : "+ mbl.mobileBrandName);
+		System.out.println("Mobile Logo : "+mbl.mobileLogo);
+		System.out.println("No of Mobile Piece : "+mbl.noOfMobilePiece);
+		System.out.println("Model Number : "+mbl.modelNumber);
+		System.out.println("Mobile IMEI Number : "+mbl.mobileImeiNumber);
+		System.out.println("Mobile Price : "+mbl.mobilePrice);
+		System.out.println("Is mobile damaged : "+mbl.isDamaged);
 	}
 
 }
